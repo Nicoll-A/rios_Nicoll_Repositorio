11b8ecd08e216a24943a859fb53d1fc77c690292
nicollrd <nicollrd>
2026-09-21 11:48:19 +0200

    Exercici fet a classe

--- Ex.c
+++ Ex.c
@@ -1,1 +1,16 @@
-#include...
+#include <stdio.h>
+
+int main() {
+	int dia;
+	int mes;
+	int any;
+	printf("\nEscribe tu fecha de nacimiento de la siguiente manera 01 / 07 / 2003\n");
+	scanf_s("%d", &dia);
+	printf("/");
+	scanf_s("%d", &mes);
+	printf("/");
+	scanf_s("%d", &any);
+
+	printf("\n%d + %d + %d = %d ", dia, mes, any, dia + mes + any);
+
+}
