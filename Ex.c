cef836d76f35163aa6e9f5b082226140632302d5
nicollrios <nicooll>
2026-09-23 21:38:40 +0200

    Quitar slash

--- Ex.c
+++ Ex.c
@@ -6,9 +6,7 @@ int main() {
 	int any;
 	printf("\nEscribe tu fecha de nacimiento de la siguiente manera 01 / 07 / 2003\n");
 	scanf_s("%d", &dia);
-	printf("/");
 	scanf_s("%d", &mes);
-	printf("/");
 	scanf_s("%d", &any);
 
 	if (any < 1000 || any>2026) {
